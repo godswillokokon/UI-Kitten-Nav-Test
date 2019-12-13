6c292ee0884d612844c95effab83f4a7a75df260
godswillokokon <godswillokokon3@gmail.com>
2019-12-13 08:15:12 +0100

    fixed drawer and sign in auto fill save

--- src/navigation/auth.navigator.tsx
+++ src/navigation/auth.navigator.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import { RouteProp } from '@react-navigation/core';
+import { StackNavigationProp } from '@react-navigation/stack';
 import { AppRoute } from './app-routes';
-import { SignInScreen, SignUpScreen, ResetPasswordScreen } from '../scenes/auth';
 
-const Stack = createStackNavigator();
+type AuthNavigatorParams = {
+  [AppRoute.SIGN_IN]: undefined;
+  [AppRoute.SIGN_UP]: undefined;
+  [AppRoute.RESET_PASSWORD]: undefined;
+}
 
-export const AuthNavigator = (): React.ReactElement => (
-  <Stack.Navigator headerMode='none'>
-    <Stack.Screen name={AppRoute.SIGN_IN} component={SignInScreen} />
-    <Stack.Screen name={AppRoute.SIGN_UP} component={SignUpScreen} />
-    <Stack.Screen name={AppRoute.RESET_PASSWORD} component={ResetPasswordScreen} />
-  </Stack.Navigator>
-);
+export interface SignInScreenProps {
+  navigation: StackNavigationProp<AuthNavigatorParams, AppRoute.SIGN_IN>;
+  route: RouteProp<AuthNavigatorParams, AppRoute.SIGN_IN>;
+}
+
+export interface SignUpScreenProps {
+  navigation: StackNavigationProp<AuthNavigatorParams, AppRoute.SIGN_UP>;
+  route: RouteProp<AuthNavigatorParams, AppRoute.SIGN_UP>;
+}
+
+export interface ResetPasswordScreenProps {
+  navigation: StackNavigationProp<AuthNavigatorParams, AppRoute.RESET_PASSWORD>;
+  route: RouteProp<AuthNavigatorParams, AppRoute.RESET_PASSWORD>;
+}
