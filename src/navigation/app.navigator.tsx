ab28d6c2c143024e665a085f915c23625f0b0e98
godswillokokon <godswillokokon3@gmail.com>
2019-12-13 07:29:08 +0100

    set up app nav

--- src/navigation/app.navigator.tsx
+++ src/navigation/app.navigator.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { WelcomeScreen } from '../scenes/welcome.component';
+import { AuthNavigator } from './auth.navigator';
 import { AppRoute } from './app-routes';
 
 const Stack = createStackNavigator();
 
-export const AppNavigator = (props): React.ReactElement => (
-  <Stack.Navigator {...props} headerMode='none'>
-    <Stack.Screen name={AppRoute.AUTH} component={WelcomeScreen}/>
+export const AppNavigator = (): React.ReactElement => (
+  <Stack.Navigator headerMode='none'>
+    <Stack.Screen name={AppRoute.AUTH} component={AuthNavigator} />
   </Stack.Navigator>
 );
