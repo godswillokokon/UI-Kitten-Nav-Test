b23b4dba50858c53abadf7f7c4109e3c920414b5
godswillokokon <godswillokokon3@gmail.com>
2019-12-13 07:47:27 +0100

    include todo nav to app routing

--- src/navigation/app.navigator.tsx
+++ src/navigation/app.navigator.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { AuthNavigator } from './auth.navigator';
+import { TodoNavigator } from './todo.navigator';
 import { AppRoute } from './app-routes';
 
 const Stack = createStackNavigator();
 
-export const AppNavigator = (): React.ReactElement => (
-  <Stack.Navigator headerMode='none'>
+export const AppNavigator = (props): React.ReactElement => (
+  <Stack.Navigator {...props} headerMode='none'>
     <Stack.Screen name={AppRoute.AUTH} component={AuthNavigator} />
+    <Stack.Screen name={AppRoute.HOME} component={TodoNavigator} />
   </Stack.Navigator>
 );
